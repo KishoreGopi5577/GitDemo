9eda7047066a1a30976d11ca20edf1228ac89488
KishoreGopi5577 <gopikishore5577@gmail.com>
2024-03-28 18:50:38 +0530

    Second commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -7,11 +7,13 @@ public class RESTAPITest {
 	@Test
 	public void postJira() {
 		System.out.println("Rest1");
+		System.out.println("Rest1GIT");
 	}
 	
 	@Test
 	public void deleteTwitter() {
 		System.out.println("Rest2");
+		System.out.println("Rest2GIT");
 	}
 
 }
